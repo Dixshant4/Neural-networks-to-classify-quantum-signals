bd196d5f7e31801664d9e5267fbd4b49d3bcbadd
Dixshant shree shreemal <90293938+Dixshant4@users.noreply.github.com>
2024-10-16 01:15:21 -0400

    Add files via upload

---
+++ multi_model_simulation_32/Model27/get_power_readings.py
@@ -0,0 +1,22 @@
+import numpy as np
+import sys
+
+sys.path.append('/home/dixshant/Multi_model_sim_32_d')
+from create_sensing_matrix import sensing_matrix
+import create_simulated_images
+
+sensing_matrix = sensing_matrix(32,32)
+
+def transform_dataset(dataset, matrix):
+    transformed_dataset = []
+    for vector, label in dataset:
+        transformed_vector = np.dot(matrix, vector)  # Matrix-vector multiplication
+        transformed_dataset.append((transformed_vector, label))
+    return transformed_dataset
+
+# Transform the dataset
+dataset = create_simulated_images.data_set_27
+transformed_dataset = transform_dataset(dataset, sensing_matrix)
+# print(transformed_dataset)
+
+# print(transformed_dataset)
